86766c063a7d34ab7e6f3cd5d5be3054448168af
CJskii <czareczarek3@gmail.com>
2023-08-13 00:42:52 +0100

    remove old imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,4 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Navbar from "../components/Navbar";
-import ONFTBridge from "../components/ONFT/Main";
-import Image from "next/image";
 import Minting from "../components/Minter/Minting";
 
 const Home: NextPage = () => {
