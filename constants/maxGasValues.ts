d6bd160c0d962aeac752fdc698d2420f12336f27
CJskii <czareczarek3@gmail.com>
2023-10-10 22:21:19 +0100

    update maxGasValues

--- constants/maxGasValues.ts
+++ constants/maxGasValues.ts
@@ -6,93 +6,93 @@ interface MaxGasValues {
 
 export const maxGasValues: MaxGasValues = {
   arbitrum: {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   avalanche: {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   bsc: {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   fantom: {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   optimism: {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   sepolia: {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   celo: {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   moonbeam: {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   moonriver: {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   zora: {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   canto: {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   harmony: {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   zksync: {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   coredao: {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   mantle: {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   metis: {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   polygon: {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   linea: {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   base: {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   "arbitrum-goerli": {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   "bsc-testnet": {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   "linea-testnet": {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   "optimism-goerli": {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   mumbai: {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   "zkevm-testnet": {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   "mantle-testnet": {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   "metis-testnet": {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   "coreDao-testnet": {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   goerli: {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
   zkevm: {
-    adapterParamMaxGas: 0.24,
+    adapterParamMaxGas: 0.1,
   },
 };
