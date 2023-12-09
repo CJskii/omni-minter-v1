1d970d43d83019fb3ca23fbd88e74c57a2201c3a
CJskii <czareczarek3@gmail.com>
2023-12-09 18:24:10 +0000

    updated contract address getters

---
+++ common/utils/getters/contracts/wNFT.ts
@@ -0,0 +1,24 @@
+export const getNFTEnvVarName = (fromNetwork: string) => {
+  switch (fromNetwork) {
+    case "BSC":
+      return process.env.NEXT_PUBLIC_BSC_NFT_CONTRACT_ADDRESS;
+    case "AVALANCHE":
+      return process.env.NEXT_PUBLIC_AVALANCHE_NFT_CONTRACT_ADDRESS;
+    case "FANTOM":
+      return process.env.NEXT_PUBLIC_FANTOM_NFT_CONTRACT_ADDRESS;
+    case "CELO":
+      return process.env.NEXT_PUBLIC_CELO_NFT_CONTRACT_ADDRESS;
+    case "MOONBEAM":
+      return process.env.NEXT_PUBLIC_MOONBEAM_NFT_CONTRACT_ADDRESS;
+    case "OPTIMISM":
+      return process.env.NEXT_PUBLIC_OPTIMISM_NFT_CONTRACT_ADDRESS;
+    case "KLAYTN":
+      return process.env.NEXT_PUBLIC_KLAYTN_NFT_CONTRACT_ADDRESS;
+    case "ARBITRUM":
+      return process.env.NEXT_PUBLIC_ARBITRUM_NFT_CONTRACT_ADDRESS;
+    case "POLYGON":
+      return process.env.NEXT_PUBLIC_POLYGON_NFT_CONTRACT_ADDRESS;
+    default:
+      return "";
+  }
+};
